80100c94877178be7c87f0c6891ed793051cfa7b
Juan Francisc Perez <jperez@tec.mx>
2025-09-04 14:45:21 -0600

    Ciclos Tarea 3

--- assignments/07CantidadPares/src/exercise.py
+++ assignments/07CantidadPares/src/exercise.py
@@ -1,14 +1,6 @@
 def main():
     #escribe tu código abajo de esta línea
-    numero = int(input())
-
-    totalPares = 0
-    while (numero >= 0):
-        if (numero % 2 == 0):
-            totalPares += 1
-        numero = int(input())
-        
-    print (f"Total de pares={totalPares}")
+    pass
 
 if __name__=='__main__':
     main()
